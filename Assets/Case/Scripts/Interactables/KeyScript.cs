9fc188958afac292cb96c5df3ea8463a2408867a
OrhanUnal <140164675+OrhanUnal@users.noreply.github.com>
2026-01-30 20:03:57 +0300

    Changing keys color with respect to their type

--- Assets/Case/Scripts/Interactables/KeyScript.cs
+++ Assets/Case/Scripts/Interactables/KeyScript.cs
@@ -2,10 +2,6 @@ using UnityEngine;
 
 public class KeyScript : MonoBehaviour, IInteractable
 {
-    string IInteractable.GetInteractionPrompt()
-    {
-        return $"[PRESS E] Take {m_CurrentType}";
-    }
     public enum KeyTypes
     {
         redKey,
@@ -13,8 +9,19 @@ public class KeyScript : MonoBehaviour, IInteractable
         blueKey,
         None
     }
-    [SerializeField] private KeyTypes m_CurrentType = KeyTypes.None;
 
+    [SerializeField] private KeyTypes m_CurrentType = KeyTypes.None;
+    [SerializeField] private MeshRenderer m_KeyRenderer;
+
+    private void Start()
+    {
+        UpdateKeyColor();
+    }
+
+    string IInteractable.GetInteractionPrompt()
+    {
+        return $"[PRESS E] Take {m_CurrentType}";
+    }
     void IInteractable.InteractLogicHold()
     {
         return;
@@ -23,5 +30,37 @@ public class KeyScript : MonoBehaviour, IInteractable
     {
         InventoryManager.Instance.AddItem(m_CurrentType);
         Destroy(gameObject);
+    }
+
+    private void UpdateKeyColor()
+    {
+        if (m_KeyRenderer == null)
+        {
+            m_KeyRenderer = GetComponentInChildren<MeshRenderer>();
+        }
+
+        if (m_KeyRenderer != null)
+        {
+            Color keyColor = GetColorForKeyType(m_CurrentType);
+            
+            Material keyMaterial = new Material(m_KeyRenderer.sharedMaterial);
+            keyMaterial.color = keyColor;
+            m_KeyRenderer.material = keyMaterial;
+        }
+    }
+
+    private Color GetColorForKeyType(KeyTypes keyType)
+    {
+        switch (keyType)
+        {
+            case KeyTypes.redKey:
+                return Color.red;
+            case KeyTypes.blueKey:
+                return Color.blue;
+            case KeyTypes.greenKey:
+                return Color.green;
+            default:
+                return Color.white;
+        }
     }
 }
