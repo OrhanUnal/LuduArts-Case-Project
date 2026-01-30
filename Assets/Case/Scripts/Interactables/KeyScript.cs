788fcb615ad2564afef9ed477eaf0e66c4527ceb
OrhanUnal <140164675+OrhanUnal@users.noreply.github.com>
2026-01-30 16:14:31 +0300

    Add key system

--- Assets/Case/Scripts/Interactables/KeyScript.cs
+++ Assets/Case/Scripts/Interactables/KeyScript.cs
@@ -2,12 +2,22 @@ using UnityEngine;
 
 public class KeyScript : MonoBehaviour, IInteractable
 {
+    public enum KeyTypes
+    {
+        redKey,
+        greenKey, 
+        blueKey,
+        None
+    }
+    [SerializeField] private KeyTypes m_CurrentType = KeyTypes.None;
+
     void IInteractable.InteractLogicHold()
     {
         return;
     }
     void IInteractable.InteractLogicButton() 
     {
-        Debug.Log("TOOK KEY");
+        InventoryManager.Instance.AddItem(m_CurrentType);
+        Destroy(gameObject);
     }
 }
